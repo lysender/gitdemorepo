7255ff7e7c586fe6c3dc30ebe856ed93c695928e
Leonel Baer <lbaer@usautoparts.com>
2014-08-28 15:04:04 +0800

    Added some cool stuff

--- application/controllers/SearchController.php
+++ application/controllers/SearchController.php
@@ -2,9 +2,18 @@
 
 class SearchController extends Zend_Controller_Action {
 
+    protected $isCool = false;
+
+    public function init()
+    {
+        parent::init();
+        $this->isCool = true;        
+    }
+
     public function indexAction()
     {
         $this->view->title = "This is the title";
         $this->view->description = "This is the description";
+        $this->view->isCool = $this->isCool;
     }
 }
