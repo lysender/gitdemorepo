246f0842ad2c37ed577872d735cb1cca694f3947
Leonel Baer <lbaer@usautoparts.com>
2014-08-28 14:12:50 +0800

    Updated sku controller

--- application/controllers/SkuController.php
+++ application/controllers/SkuController.php
@@ -6,5 +6,7 @@ class SkuController extends Zend_Controller_Action {
     {
         $this->view->title = "This is the title";
         $this->view->description = "This is the description";
+
+        $this->view->product = array(1, 2, 3);
     }
 }
