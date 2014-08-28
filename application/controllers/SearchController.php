11be5f930472ecb8be7f6155f1f8d03a4e1c04e9
Leonel Baer <lbaer@usautoparts.com>
2014-08-28 16:12:40 +0800

    Revert "Added search controller content - feature-002-search"
    This reverts commit 5a907d3c609333abbd8b8ad893687ead1a71f8cd.

--- application/controllers/SearchController.php
+++ application/controllers/SearchController.php
@@ -4,7 +4,6 @@ class SearchController extends Zend_Controller_Action {
 
     public function indexAction()
     {
-        $this->view->title = "This is the title";
-        $this->view->description = "This is the description";
+        
     }
 }
