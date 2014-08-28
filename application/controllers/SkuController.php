f14b1b44bdbef4aef2330d2a3f7453b25c493da8
Leonel Baer <lbaer@usautoparts.com>
2014-08-28 11:30:02 +0800

    Added sku controller content

--- application/controllers/SkuController.php
+++ application/controllers/SkuController.php
@@ -4,6 +4,7 @@ class SkuController extends Zend_Controller_Action {
 
     public function indexAction()
     {
-        
+        $this->view->title = "This is the title";
+        $this->view->description = "This is the description";
     }
 }
