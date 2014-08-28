de7b3e55c0199ad249d19d9c9b87ec3ad6e36007
Leonel Baer <lbaer@usautoparts.com>
2014-08-28 11:17:15 +0800

    Added index controller content

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -4,6 +4,7 @@ class IndexController extends Zend_Controller_Action {
 
     public function indexAction()
     {
-        
+        $this->view->title = "This is the title";
+        $this->view->description = "This is the description";
     }
 }
