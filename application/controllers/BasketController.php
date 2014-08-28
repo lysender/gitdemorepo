f175731e7ab89739e8a2a04afb132197fe9b4bc6
Leonel Baer <lbaer@usautoparts.com>
2014-08-28 14:18:57 +0800

    Updated basket contents

--- application/controllers/BasketController.php
+++ application/controllers/BasketController.php
@@ -7,6 +7,8 @@ class BasketController extends Zend_Controller_Action {
         $this->view->title = "This is the title";
         $this->view->description = "This is the description";
 
-        $this->view->products = array(1, 2, 3);
+        $this->view->products = array(
+            array(1, 2, 3),
+        );
     }
 }
