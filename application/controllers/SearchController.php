ea3a47df283a1a39da282d17f67fdcfefda42168
Leonel Baer <lbaer@usautoparts.com>
2014-08-28 16:24:27 +0800

    Added search keyword

--- application/controllers/SearchController.php
+++ application/controllers/SearchController.php
@@ -6,5 +6,6 @@ class SearchController extends Zend_Controller_Action {
     {
         $this->view->title = "This is the title";
         $this->view->description = "This is the description";
+        $this->view->keyword = true;
     }
 }
