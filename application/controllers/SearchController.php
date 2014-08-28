fe446fb669643d06b95759d7a2b7a5cba2d640ba
Leonel Baer <lbaer@usautoparts.com>
2014-08-28 16:07:15 +0800

    Revert "Added some cool stuff"
    This reverts commit 7255ff7e7c586fe6c3dc30ebe856ed93c695928e.

--- application/controllers/SearchController.php
+++ application/controllers/SearchController.php
@@ -2,18 +2,9 @@
 
 class SearchController extends Zend_Controller_Action {
 
-    protected $isCool = false;
-
-    public function init()
-    {
-        parent::init();
-        $this->isCool = true;        
-    }
-
     public function indexAction()
     {
         $this->view->title = "This is the title";
         $this->view->description = "This is the description";
-        $this->view->isCool = $this->isCool;
     }
 }
